a896bdce0ad0063cb0fc77555dbc8a0898fd893a
Lê Thái Sơn <lethaison04011999@gmail.com>
2021-01-04 20:29:00 +0700

    first commit

---
+++ src/components/pages/about/About.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h6 className="mt-bottom">
+            <strong>ABOUT ME</strong>
+          </h6>
+          <p className="grey-text">
+         
+I am a student at Danang Pedagogical University majoring in information technology. I spent 3 years learning java to follow the path of Java web programming to become a good developer
+          </p>
+        </div>
+        <div className="card-action">
+          <h6>
+            <strong>PERSONAL INFO</strong>
+          </h6>
+          <div className="row mt">
+            <div className="col xl6 l6 m6 s12">
+              <p>
+                <strong>Address:</strong> 05 Ngo Sy Lien ,Lien Chieu , Da Nang
+              </p>
+              <p>
+                <strong>Email:</strong> lethaison04011999@gmail.com
+              </p>
+              <p>
+                <strong>Phone:</strong> 0379046120
+              </p>
+            </div>
+            <div className="col xl6 l6 m6 s12">
+              <p>
+                <strong>Main Language</strong> - English
+              </p>
+              <p>
+                <strong>Date of birth </strong> - January 4, 1999
+              </p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/pages/experiences/Experiences.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default function Experiences() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong>EXPERIENCE</strong>
+          </h6>
+          <div className="row mt-top">
+            <div className="col xl4 l4 m6 s12">
+              <p className="teal year_exp white-text">
+                Jan
+                <strong>2017</strong> - Mar
+                <strong>2018</strong>
+              </p>
+            </div>
+            <div className="col xl8 l8 m6 s12">
+              <blockquote className="no-pad">
+                <h6 className="no-pad mt-bottom">
+                  <strong>Java Dev</strong>
+                </h6>
+                <p>
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                Sed placerat posuere mi vitae consequat. 
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                Maecenas in lacinia enim, eu consectetur urna. Donec aliquet congue eros ut vulputate. 
+                In nec quam mauris.
+
+
+                </p>
+              </blockquote>
+            </div>
+          </div>
+          <div className="row mt-top">
+            <div className="col xl4 l4 m6 s12">
+              <p className="teal year_exp white-text">
+                Jan
+                <strong>2018</strong> - Mar
+                <strong>2020</strong>
+              </p>
+            </div>
+            <div className="col xl8 l8 m6 s12">
+              <blockquote className="no-pad">
+                <h6 className="no-pad mt-bottom">
+                  <strong>Web Java</strong>
+                </h6>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+                  earum doloribus dicta quod architecto, praesentium totam
+                  molestiae similique culpa repellat blanditiis nam facilis eius
+                  quaerat distinctio vitae
+                </p>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
